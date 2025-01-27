cf06b470de3d7e0e822d7324bb0e0ac85c5b5c95
Irham Rahmat Saleh <irhamrahmatsaleh1997@gmail.com>
2025-01-27 21:44:25 +0700

    Remove dayjs script tag from checkout.html and import dayjs in checkout.js for improved module handling; export formatCurrency function from utils/money.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import { cart, removeFromCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrnecy } from './utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
 hello();
 
